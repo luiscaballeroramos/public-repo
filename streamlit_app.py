50c1d708d1150f79e6a9b1bc751d18fb6c23b022
luiscaballeroramosDOSA <lcaballeror@dragadosoffshore.es>
2025-06-13 08:32:19 +0200

    try private

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,33 @@ import subprocess
 import sys
 import os
 import site
+from dotenv import load_dotenv
 
+load_dotenv()
+
+token = os.getenv("GITHUB_TOKEN")
+user = os.getenv("GITHUB_USER")
+if not token or not user:
+    raise EnvironmentError("Faltan GITHUB_TOKEN o GITHUB_USER en el archivo .env")
+
+repo_url = f"git+https://{token}@github.com/{user}/private-repo.git#egg=private_module"
 LOCAL_PACKAGE_DIR = os.path.join(os.path.dirname(__file__), "external_packages")
 os.makedirs(LOCAL_PACKAGE_DIR, exist_ok=True)
-
-repo_url = "git+https://<TOKEN_PERSONAL>@github.com/luiscaballeroramos/private-repo.git#egg=private_module"
 
 try:
     from private_module.core import private_function
 except ImportError:
-    subprocess.check_call([
-        sys.executable, "-m", "pip", "install", "--target", LOCAL_PACKAGE_DIR, repo_url
-    ])
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--target",
+            LOCAL_PACKAGE_DIR,
+            repo_url,
+        ]
+    )
     sys.path.append(LOCAL_PACKAGE_DIR)
     from private_module.core import private_function
 
