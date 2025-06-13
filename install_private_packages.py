a23dff6882207c268a25a9d177a9f791e3342c25
luiscaballeroramosDOSA <lcaballeror@dragadosoffshore.es>
2025-06-13 10:40:57 +0200

    refactor: streamline environment variable checks and ensure private package installation

--- install_private_packages.py
+++ install_private_packages.py
@@ -3,13 +3,10 @@ import sys
 import subprocess
 from dotenv import load_dotenv
 
+TOKENS = {"private-repo": ("GITHUB_TOKEN", "GITHUB_USER")}
+PACKAGES_MODULES = {"private-repo": ["private_module"]}
 # Cargar variables de entorno
 load_dotenv()
-token = os.getenv("GITHUB_TOKEN")
-user = os.getenv("GITHUB_USER")
-
-if not token or not user:
-    raise EnvironmentError("Faltan GITHUB_TOKEN o GITHUB_USER en el archivo .env")
 
 # Carpeta de instalación local
 package_dir = os.path.join(os.path.dirname(__file__), "external_packages")
@@ -19,6 +16,10 @@ sys.path.append(package_dir)
 
 # Función reutilizable para asegurar que el módulo esté disponible
 def ensure_private_package(module_name: str, repo_name: str):
+    token = os.getenv(TOKENS[repo_name][0])
+    user = os.getenv(TOKENS[repo_name][1])
+    if not token or not user:
+        raise EnvironmentError("Faltan GITHUB_TOKEN o GITHUB_USER en el archivo .env")
     try:
         __import__(module_name)
     except ImportError:
@@ -32,4 +33,6 @@ def ensure_private_package(module_name: str, repo_name: str):
 
 
 # Asegurar paquete privado (reutilizable)
-ensure_private_package("private_module", "private-repo")
+for repo, modules in PACKAGES_MODULES.items():
+    for module in modules:
+        ensure_private_package(module, repo)
